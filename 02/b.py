3c9ae3d6f09f2d9144d37e7e78e7b38ca104a8b1
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-12-05 20:08:27 +0100

    02
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

---
+++ 02/b.py
@@ -0,0 +1,18 @@
+import re
+
+inp = tuple(map(lambda ab: tuple(map(int, ab.split('-'))),
+                open('inp_my.txt').readline().strip().split(',')))
+print(inp)
+
+pat = re.compile(r'^(\d+)\1+$')
+findings=[]
+
+for f,t in inp:
+    print(f'{f}-{t}')
+    for n in range(f, t+1):
+        s = str(n)
+        # print(repr(s))
+        if pat.match(s):
+            print('found', s)
+            findings.append(n)
+print('sum:', sum(findings))

c450a6982ae82bdc7a11ae6914ec48ec788df0b4
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-12-14 11:22:02 +0100

    09
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 09/b.py
+++ 09/b.py
@@ -11,24 +11,18 @@ def area(a: Point, b: Point) -> float:
 
 inp = tuple(Point(*map(int, l.strip().split(','))) for l in open('inp_mu.txt.bin').readlines())
 
-areas: list[tuple[Point, Point, float]] = [(a, b, area(a, b)) for a, b in combinations(inp, 2)]
-areas.sort(key=lambda d: d[2], reverse=True)
+rects: list[tuple[Point, Point, float]] = [(a, b, area(a, b)) for a, b in combinations(inp, 2)]
+rects.sort(key=lambda d: d[2], reverse=True)
 
 horrs: list[Line] = []
 verts: list[Line] = []
 for p1, p2 in chain(pairwise(inp), ((inp[0], inp[-1]),)):
     if p1.x == p2.x:
-        if p1.y < p2.y:
-            verts.append(Line(p1, p2))
-        else:
-            verts.append(Line(p2, p1))
+        verts.append(Line(p1, p2) if p1.y < p2.y else Line(p2, p1))
     else:
-        if p1.x < p2.x:
-            horrs.append(Line(p1, p2))
-        else:
-            horrs.append(Line(p2, p1))
+        horrs.append(Line(p1, p2) if p1.x < p2.x else Line(p2, p1))
 
-for p1, p2, area in areas:
+for p1, p2, area in rects:
     x_min, x_max = min(p1.x, p2.x), max(p1.x, p2.x)
     y_min, y_max = min(p1.y, p2.y), max(p1.y, p2.y)
     if any(p1.y <= y_min < p2.y or p1.y < y_max <= p2.y

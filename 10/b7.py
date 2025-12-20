347ee35043348bda08b76397a6ba1a512f71d271
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-12-20 15:30:11 +0100

    10
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 10/b7.py
+++ 10/b7.py
@@ -1,33 +1,16 @@
+def solve(buttons, joltages):
+    return 0
+
+
 bests = []
-for inp in open('inp_ex.txt.bin'):
+for inp in open('inp_ko.txt.bin'):
     inp = inp.strip().split(' ')
-    buttons = tuple(tuple(map(int, i[1:-1].split(','))) for i in inp[1:-1])
+    buttons = list(tuple(map(int, i[1:-1].split(','))) for i in inp[1:-1])
     joltages = list(map(int, inp[-1][1:-1].split(',')))
-    joltages0 = [0] * len(joltages)
     del inp
-
-    pressed = 0
-    while joltages != joltages0:
-        joltages_ordered_pos = sorted(range(len(joltages)), key=joltages.__getitem__)
-        print(joltages_ordered_pos, joltages)
-        max_pos = joltages_ordered_pos.pop()
-        buttons_possible = tuple(b for b in buttons if max_pos in b)
-        buttons_possible_longest = max(len(b) for b in buttons_possible)
-        buttons_possible = tuple(b for b in buttons_possible if len(b) == buttons_possible_longest)
-        if len(buttons_possible) == 0:
-            raise ValueError('buttons_possible == 0')
-        while len(buttons_possible) > 1:
-            print('buttons_possible', buttons_possible)
-            #
-            # wtf ?
-
-        button = buttons_possible[0]
-        presses = min(joltages[j] for j in button)
-        for j in button:
-            joltages[j] -= presses
-        pressed += pressed
-    print(pressed)
-    bests.append(pressed)
+    best = solve(buttons, joltages)
+    print('best', best)
+    bests.append(best)
 
 print('bests', repr(bests))
 print('res', sum(bests))

9b9ab2f50988657861814384715585adbf45aee4
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-01-11 22:07:23 +0100

    11
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- 11/b3.py
+++ 11/b3.py
@@ -8,31 +8,38 @@ D_OUT = 'out'
 D_DAC = 'dac'
 D_FFT = 'fft'
 
-inp = {k: v for k, v in map(inp2connections, open('inp_ko.txt.bin').readlines())}
+inp: dict[str, tuple[str, ...]] = {k: v for k, v in map(inp2connections, open('inp_mu.txt.bin').readlines())}
 inp[D_OUT] = tuple()
-# print('inp', repr(inp))
 
-inp_rev: dict[str, list[str]] = {k: [] for k in inp}
-for f, ts in inp.items():
-    for t in ts:
-        inp_rev[t].append(f)
+visited = []
+targets: dict[str, dict[str, int]] = {k: dict.fromkeys(inp.keys(), 0) for k, v in inp.items()}
 
+todos: list[list[str]] = [[D_SVR]]
+while todos:
+    todo = todos.pop()
+    for todo_n in inp[todo[-1]]:
+        for t in todo:
+            targets[t][todo_n] += 1
+        if todo_n in visited:
+            for t in todo:
+                for k, v in targets[todo_n].items():
+                    targets[t][k] += v
+            continue
+        visited.append(todo_n)
+        todo_nn = todo[:]
+        todo_nn.append(todo_n)
+        todos.append(todo_nn)
+print('targets', repr(targets))
 
-def find_ways(fts, start, end):
-    ways = 0
-    todos: set[str] = {start, }
-    while todos:
-        print(repr(todos))
-        todo = todos.pop()
-        for todo_n in fts[todo]:
-            if todo_n == end:
-                ways += 1
-                continue
-            todos.append(todo_n)
-    return ways
-
-print(
-    (find_ways(inp, D_SVR, D_DAC) * find_ways(inp, D_DAC, D_FFT) * find_ways(inp, D_FFT, D_OUT))
-    +
-    (find_ways(inp, D_SVR, D_FFT) * find_ways(inp, D_FFT, D_DAC) * find_ways(inp, D_DAC, D_OUT))
-)
+if targets[D_DAC][D_FFT] > 0:
+    print('res D_DAC -> D_FFT',
+          targets[D_SVR][D_DAC] *
+          targets[D_DAC][D_FFT] *
+          targets[D_FFT][D_OUT])
+elif targets[D_FFT][D_DAC] > 0:
+    print('res D_FFT -> D_DAC',
+          targets[D_SVR][D_FFT] *
+          targets[D_FFT][D_DAC] *
+          targets[D_DAC][D_OUT])
+else:
+    print('kapott', repr(targets))
